0435c847b7074a2288d7508c0eb63d32b5a570e7
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-01-31 08:58:16 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,5 @@
-
+game = {
+  A: new OmegaNum(10), //Ammount of A you have
+  ADimensions: [null, "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th"], //A Dimension Names (added null for less confusion)
+  ADimCost: [null, new OmegaNum(10), new OmegaNum(100), new OmegaNum(10000), new OmegaNum(1e6),new OmegaNum(1e8), new OmegaNum(1e10), new OmegaNum(1e12), new OmegaNum(1e15)], // A Dimemsion Costs
+}
