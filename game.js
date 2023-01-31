48215c0d2a076223996c01d9f6a187b264cb38f1
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-01-31 09:53:16 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,14 @@
 game = {
-  A: new OmegaNum(10), //Ammount of A you have
+  A: new OmegaNum(10), //Ammount of A you have,
+  APerSec: new OmegaNum(0),
   ADimensions: [null, "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th"], //A Dimension Names (added null for less confusion)
   ADimCost: [null, new OmegaNum(10), new OmegaNum(100), new OmegaNum(10000), new OmegaNum(1e6),new OmegaNum(1e8), new OmegaNum(1e10), new OmegaNum(1e12), new OmegaNum(1e15)], // A Dimemsion Costs
+  ADim1: new OmegaNum(0),
+  ADim2: new OmegaNum(0),
+  ADim3: new OmegaNum(0),
+  ADim4: new OmegaNum(0),
+  ADim5: new OmegaNum(0),
+  ADim6: new OmegaNum(0),
+  ADim7: new OmegaNum(0),
+  ADim8: new OmegaNum(0)
 }
